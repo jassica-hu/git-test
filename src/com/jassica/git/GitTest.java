c580ae55a4d38112b08b74dc366174e0d80ddcf6
jassica-hu <488335183@qq.com>
2025-07-06 09:16:24 +0800

    pull commit

--- src/com/jassica/git/GitTest.java
+++ src/com/jassica/git/GitTest.java
@@ -6,8 +6,9 @@ public class GitTest {
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("master test！");
+        System.out.println("hot-fix test！");
+        System.out.println("push test！");
+        System.out.println("pull test！");
     }
 }
